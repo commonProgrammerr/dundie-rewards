a632af4cf5894a466657e174a8fa906b82468a64
André Escorel <andre.escorel@global.komatsu>
2025-01-11 12:44:10 -0300

    feat: add argument parser and subcommands

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,9 +1,35 @@
-import sys
+import argparse
+
+
+def load(filepath):
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}")
 
 
 def main():
-    print(f"Executing {__name__} from entry point")
-    return 0
+    parser = argparse.ArgumentParser(
+        description="Dundie Mifflin Rewards CLI",
+        epilog="Enjoy and use with caution",
+    )
+
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="Subcommand to execute",
+        choices=("load", "send", "show"),
+    )
+
+    parser.add_argument(
+        "filepath", type=str, help="Path to the file to load", default=None
+    )
+
+    args = parser.parse_args()
+    globals()[args.subcommand](args.filepath)
 
 
-sys.exit(main())
+if __name__ == "__main__":
+    main()
