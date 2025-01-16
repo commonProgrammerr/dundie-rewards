93e1b6683437cb7d6a9d3dff73c1fef183d0fbcb
André Escorel <andre.escorel@global.komatsu>
2025-01-16 12:03:50 -0300

    tests: adding unit tests for simple password generator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 from dundie.utils.email import check_valid_email
+from dundie.utils.user import generate_simple_password
 
 # List of Valid Email Addresses
 valid_emails = """\
@@ -49,3 +50,17 @@ def test_positive_check_valid_email(email):
 def test_negative_check_valid_email(email):
     """Ensure that check_valid_email returns True for valid emails"""
     assert check_valid_email(email) is False
+
+
+@pytest.mark.unit
+def test_generate_simple_password():
+    """Test generation of random simple passwords.
+
+    TODO: Generate hashed complex and encrypted passwords
+    """
+    passwords = []
+
+    for _ in range(100):
+        passwords.append(generate_simple_password())
+
+    assert len(set(passwords)) == len(passwords)
